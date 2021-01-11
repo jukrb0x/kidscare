77edce7a6f5a4a0dee0dd23e3482ac430a7adec6
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-01-12 00:22:17 +0800

    minor changes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <q-button>
+      <router-link :to="{ name: '/' }"> reset </router-link>
+    </q-button>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex"
+};
 </script>
