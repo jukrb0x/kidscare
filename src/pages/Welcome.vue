d76a22a9b980412c4451255e5b5fb80a6a9cef70
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-01-12 00:29:46 +0800

    minor changes

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,16 +1,16 @@
 <template>
   <!-- we desire for a swipe-able welcome(opening) page, but now it's all static since due date is coming -->
   <q-page padding class="welcome flex column items-center">
-    <div class="body flex column flex-center items-center">
+    <div class="body flex column flex-center items-center" @click="routerHome">
       <span class="logo-block"><img :src="logo" alt=""/></span>
       <span class="logo-text text-h4">
         <span>Kids</span>
         <span>Care</span>
       </span>
-      <span class="special">Covid-19 special</span>
+      <span class="special text-subtitle1">Covid-19 special</span>
     </div>
-    <div class="bottom">
-      abc
+    <div class="bottom flex column flex-center">
+      <div class="power"><q-icon name="cloud"></q-icon> Powered by HCI ICU</div>
     </div>
   </q-page>
 </template>
@@ -22,23 +22,34 @@ export default {
     return {
       logo: require("assets/virus.svg") // hint: use assets instead ~assets in data section
     };
+  },
+  methods: {
+    routerHome() {
+      this.$router.push({ path: "collectData" });
+    }
   }
 };
 </script>
 
 <style lang="sass" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Nunito:ital@1&display=swap')
 
 .welcome
   background-color: $grey-1
+  line-height: 35px
+
 .logo-block
   margin-top: 180px
+
 .logo-text span
   &:first-child
     color: $deep-orange-5
   &:last-child
-    color:$blue-7
+    color: $blue-7
 
-.special
-  font-family: 'Nunito', sans-serif
+.bottom
+  .power
+    position: absolute
+    align-items: center
+    bottom: 20px
+    color: $blue-grey
 </style>
