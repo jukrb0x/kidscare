18af65e49a009964758d242bba3df2e37449c4c5
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-01-12 00:29:51 +0800

    update router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,28 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/WelcomeLayout.vue'),
+    path: "/",
+    component: () => import("layouts/WelcomeLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Welcome.vue')}
+      { path: "", component: () => import("pages/Welcome.vue") },
+      { path: "collectData", component: () => import("pages/CollectData.vue") },
+      {
+        path: "collectData/kidsInfo",
+        component: () => import("pages/CollectData/KidsInfo")
+      }
     ]
   },
   {
-    path: '/home',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Home.vue') }
-    ]
+    path: "/home",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Home.vue") }]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '*',
-    component: () => import('pages/Error404.vue')
+    path: "*",
+    component: () => import("pages/Error404.vue")
   }
-]
+];
 
-export default routes
+export default routes;
