1a9b4b1d3431b94ab3c83b06281d05c73731f711
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-01-12 00:29:40 +0800

    add collect data pages

---
+++ src/pages/CollectData.vue
@@ -0,0 +1,88 @@
+<template>
+  <!-- first use the app, collect the data from users-->
+  <q-page padding class="my-gradient flex column">
+    <div class="body">
+      <!--TODO: Add logo here-->
+      <div class="title text-h2 text-blue-1">
+        Hi there!
+      </div>
+      <div class="tips text-white text-weight-regular">
+        {{ tip.l1 }}
+        <br />
+        <!--        <span style="font-size: 16px;">{{ tip.l2 }}</span>-->
+      </div>
+      <div class="btn">
+        <q-btn
+          size="18px"
+          outline
+          class="go"
+          label="Get Started"
+          @click="routerGo"
+        ></q-btn>
+        <q-btn
+          size="15px"
+          :ripple="false"
+          flat
+          class="back"
+          label="Exit"
+        ></q-btn>
+      </div>
+      <div class="indicator">
+        <q-icon class="arrow animated" name="north"></q-icon>
+
+        Click here to start!
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "CollectData",
+  data() {
+    return {
+      tip: {
+        l1:
+          "Thank you for using this app, this app will help you with the knowledge to COVID-19, the pandemic ....",
+        l2:
+          "Continuing with the next steps will indicate your acceptance of our user agreement and privacy consent."
+      }
+    };
+  },
+  methods: {
+    routerGo() {
+      this.$router.push("/collectData/kidsInfo");
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.body
+  margin-left: 10px
+.title
+  margin: 220px auto 10px 0
+.tips
+  font-size: $body-text
+  font-weight: normal
+  line-height: normal
+  margin-top: 20px
+  //margin-bottom: 45px
+.btn
+  margin-top: 25px
+  .go
+    color: $light-blue-1
+  .back
+    color: $light-blue-2
+
+.indicator
+  color: white
+  margin-top: 10px
+  margin-left: 5px
+  font-size: large
+  .arrow
+    animation: bouncer
+    //height: 10px
+    animation-duration: 1.5s
+    animation-iteration-count: infinite
+</style>
---
+++ src/pages/CollectData/KidsInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding class="my-gradient flex column">
+    <div class="body">
+      <q-btn
+        class="back"
+        size="md"
+        flat
+        color="blue-1"
+        icon="arrow_back_ios"
+        label="Back"
+        @click="routerBack"
+      ></q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "KidsInfo",
+  methods: {
+    routerBack() {
+      this.$router.push({ path: "/collectData" });
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.body
+  margin-top: 50px
+</style>
