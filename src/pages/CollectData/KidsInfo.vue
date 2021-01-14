177f56c787ab4f6710155d79d68a2738fc948ff0
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-01-14 22:46:16 +0800

    done kidsinfo page

--- src/pages/CollectData/KidsInfo.vue
+++ src/pages/CollectData/KidsInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page padding class="my-gradient flex column">
-    <div class="body">
+    <div class="header">
       <q-btn
-        class="back"
+        class="back q-my-sm"
         size="md"
         flat
         color="blue-1"
@@ -10,6 +10,83 @@
         label="Back"
         @click="routerHandler('/collectData')"
       ></q-btn>
+    </div>
+    <q-separator dark />
+    <div class="body">
+      <div class="text">
+        <div class="q-pa-sm q-gutter-sm">
+          <q-banner inline-actions rounded class="bg-blue-1 text-black-2">
+            <template v-slot:avatar>
+              <q-icon name="mdi-account-heart-outline" color="primary" />
+            </template>
+            {{ info }}
+          </q-banner>
+        </div>
+      </div>
+      <div class="q-pa-sm forms">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-sm">
+          <q-input
+            color="blue-1"
+            filled
+            v-model="me.name"
+            label="Your name *"
+            lazy-rules
+            :rules="[val => (val && val.length > 0) || 'Please type your name']"
+          />
+
+          <q-input
+            color="blue-1"
+            filled
+            type="number"
+            v-model="me.age"
+            label="Your age *"
+            lazy-rules
+            :rules="[
+              val => (val !== null && val !== '') || 'Please type e age',
+              val => (val > 0 && val < 100) || 'Please type a real age'
+            ]"
+          />
+
+          <q-input
+            color="blue-1"
+            filled
+            type="number"
+            v-model="parent.phone"
+            label="Emergency contact phone number"
+            lazy-rules
+            :rules="[
+              val =>
+                (val !== null && val !== '') ||
+                'Please type your emergency phone number',
+              val =>
+                (val > 0 && val < 100) || 'Please type a real telephone number'
+            ]"
+          />
+
+          <q-select
+            options-dark
+            color="blue-1"
+            text-color
+            filled
+            type="number"
+            v-model="parent.relation"
+            :options="parent.relations"
+            label="Emergency contact relation"
+          />
+
+          <div class="btn-group">
+            <q-btn label="Submit" type="submit" outline color="blue-1" />
+            <q-btn
+              label="Reset"
+              type="reset"
+              color="blue-1"
+              flat
+              outline
+              class="q-ml-sm"
+            />
+          </div>
+        </q-form>
+      </div>
     </div>
   </q-page>
 </template>
@@ -20,11 +97,46 @@ import RouterMixin from "components/RouterMixin";
 export default {
   name: "KidsInfo",
   mixins: [RouterMixin],
-  methods: {}
+  methods: {
+    onReset() {
+      this.me.name = null;
+      this.me.age = null;
+      this.parent.relation = null;
+      this.parent.phone = null;
+    }
+  },
+  data() {
+    return {
+      info: "Let your parents help you through!",
+      accept: false,
+      me: {
+        name: null,
+        age: null
+      },
+      parent: {
+        relation: null,
+        relations: ["Parents", "Grandparents", "Brotehr/Sister", "Others"],
+        phone: null
+      }
+    };
+  }
 };
 </script>
 
 <style lang="sass" scoped>
-.body
+.header
+  position: sticky
   margin-top: 50px
+
+.body
+  margin-left: 10px
+  margin-right: 10px
+  //color: #fff
+.text
+  margin-top: 20px
+  .text-card
+    opacity: .0
+
+.btn-group
+  margin-top: 35px
 </style>
