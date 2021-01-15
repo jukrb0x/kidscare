e494812a7c072d10b49023f5b68f9dc4d2d71e1c
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-01-15 15:10:29 +0800

    add intro page and layout (quick facts)

---
+++ src/layouts/IntroLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container class="ios-top">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "IntroLayout",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/pages/IntroFeatures.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div @click="routerHandler('/')">back</div>
+    Test
+  </q-page>
+</template>
+
+<script>
+import RouterMixin from "components/RouterMixin";
+
+export default {
+  name: "IntroFeatures",
+  mixins: [RouterMixin]
+};
+</script>
+
+<style scoped></style>
